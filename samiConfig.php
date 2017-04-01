29c0e005cfb7ee43a65af941692953269c86ca11
matthieu88160 <matthieu.vallance@cscfa.fr>
2017-04-01 10:55:39 +0000

    initial commit

---
+++ samiConfig.php
@@ -0,0 +1,15 @@
+<?php
+
+use Sami\Sami;
+use Symfony\Component\Finder\Finder;
+
+$iterator = Finder::create()
+    ->files()
+    ->name('*.php')
+    ->exclude('Tests')
+    ->in(__DIR__.'/src/CSDT/CSR4');
+
+return new Sami($iterator, array(
+    'build_dir'            => __DIR__.'/docAPI',
+    'cache_dir'            => __DIR__.'/cacheDocAPI',
+));
---
+++ test.sh
@@ -0,0 +1,79 @@
+#! /bin/bash
+
+function usage {
+  echo "Usage: $0 <PHP/PATH>" 1>&2
+  exit 1
+}
+
+if [ -z "$1" ]
+then
+    echo -e "\e[31mPHP executable path must be defined as first argument\e[0m"
+    usage
+fi
+
+STATUS=0
+TEST_RES=""
+
+PHP=$1
+
+function runner {
+    TEST_RES=`$1`
+    local TEST_RET=$?
+}
+
+function test {
+    TEST_RES=`$1`
+    local TEST_RET=$?
+    
+    if [ $TEST_RET != 0 ]
+    then
+        echo -e "\e[31m$2 FAILED\e[0m"
+        
+        echo "$TEST_RES"
+        
+        STATUS=$((STATUS + $3))
+    else
+        echo -e "\e[32m$2 SUCCESS\e[0m"
+    fi
+}
+
+if [ ! -d "doc" ]
+then
+    mkdir -p "doc"
+fi
+
+runner "$PHP vendor/bin/phpcbf src/"
+echo "$TEST_RES" > doc/phpcbf.txt
+
+test "$PHP vendor/bin/phpunit" PHPUnit 100
+echo "$TEST_RES" > doc/phpunit.txt
+
+test "$PHP vendor/bin/phpcs --standard=PSR2 src/" PHPCS 100
+echo "$TEST_RES" > doc/phpcs.txt
+
+test "$PHP vendor/bin/phpmd src/ text ./phpmd.xml" PHPMD 100
+echo "$TEST_RES" > doc/phpmd.txt
+
+test "$PHP vendor/bin/phpcpd src/" PHPCPD 1
+echo "$TEST_RES" > doc/phpcpd.txt
+
+test "$PHP vendor/bin/sami.php update samiConfig.php" SAMI 1
+echo "$TEST_RES" > doc/phpcpd.txt
+
+runner "$PHP vendor/bin/phpcs src/"
+echo "$TEST_RES" > doc/phpcs_all.txt
+
+if [ "$STATUS" -eq 0 ]
+then
+    echo -e "\n\e[42m"
+    echo -e "\e[30mTHE STATUS IS STABLE\n\e[0m\n\e[49m"
+elif [ "$STATUS" -lt 100 ]
+then
+    echo -e "\n\e[43m"
+    echo -e "\e[30mTHE STATUS IS UNSTABLE\n\e[0m\n\e[49m"
+else
+    echo -e "\n\e[41m"
+    echo -e "\e[30mTHE STATUS IS FAILURE\n\e[0m\n\e[49m"
+fi
+
+exit $STATUS
